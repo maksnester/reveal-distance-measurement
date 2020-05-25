66ec1a610d2d79016bb420994846b9806f37cab2
Maksim Nesterenko <maksim.nesterenko@cognite.com>
2020-05-25 16:53:29 +0200

    add points when model is clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import React, { useEffect, useRef } from "react";
 import { CogniteClient } from "@cognite/sdk";
 import * as THREE from "three";
-import { CadNode, RevealManager } from "@cognite/reveal/experimental";
+import {
+  CadNode,
+  intersectCadNodes,
+  RevealManager,
+} from "@cognite/reveal/experimental";
 import CameraControls from "camera-controls";
 import { Scene, WebGLRenderer } from "three";
 
 CameraControls.install({ THREE });
+
+function getNormalizedCoords(
+  event: MouseEvent,
+  domElement: HTMLCanvasElement
+): { x: number; y: number } {
+  const rect = domElement.getBoundingClientRect();
+  const x = ((event.clientX - rect.left) / domElement.clientWidth) * 2 - 1;
+  const y = ((event.clientY - rect.top) / domElement.clientHeight) * -2 + 1;
+  return { x, y };
+}
+
+function createSphere(point: THREE.Vector3, color: string): THREE.Mesh {
+  const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.2),
+    new THREE.MeshPhongMaterial({ color })
+  );
+  sphere.position.copy(point);
+  return sphere;
+}
 
 function App() {
   const canvasWrapper = useRef<HTMLDivElement>(null);
@@ -32,6 +55,10 @@ function App() {
       );
       scene.add(model);
 
+      // without light there is no colors for custom geometry (like our spheres)
+      var light = new THREE.AmbientLight( 0xffffff ); // soft white light
+      scene.add( light );
+
       const renderer = new THREE.WebGLRenderer();
       const width = window.innerWidth / 2;
       const height = window.innerHeight / 2;
@@ -57,19 +84,49 @@ function App() {
 
       const clock = new THREE.Clock();
 
+      let pickingNeedsUpdate = false;
+
       const render = () => {
         const delta = clock.getDelta();
         const controlsNeedUpdate = controls.update(delta);
         if (controlsNeedUpdate) {
           revealManager.update(camera);
         }
-        if (controlsNeedUpdate || modelsNeedUpdate) {
+        if (controlsNeedUpdate || modelsNeedUpdate || pickingNeedsUpdate) {
+          console.log(
+            "controlsNeedUpdate || modelsNeedUpdate || pickingNeedsUpdate",
+            controlsNeedUpdate,
+            modelsNeedUpdate,
+            pickingNeedsUpdate
+          );
           renderer.render(scene, camera);
+          modelsNeedUpdate = false;
+          pickingNeedsUpdate = false;
         }
         requestAnimationFrame(render);
       };
       revealManager.update(camera);
       render();
+
+      renderer.domElement.addEventListener("click", (event: MouseEvent) => {
+        const coords = getNormalizedCoords(event, renderer.domElement);
+
+        // Pick in Reveal
+        const revealPickResult = (() => {
+          const intersections = intersectCadNodes([model], {
+            renderer,
+            camera,
+            coords,
+          });
+          return intersections[0];
+        })();
+
+        if (revealPickResult) {
+          scene.add(createSphere(revealPickResult.point, "#f5f500"));
+          model.requestNodeUpdate([revealPickResult.treeIndex]);
+          pickingNeedsUpdate = true;
+        }
+      });
     })();
 
     return () => {
