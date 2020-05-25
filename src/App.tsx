5533d28c825730d283eb53ed46fa6c6506636cdd
Maksim Nesterenko <maksim.nesterenko@cognite.com>
2020-05-25 09:20:37 +0200

    shows some model from 3d demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { Cognite3DViewer } from '@cognite/reveal'
+import { Cognite3DModel, Cognite3DViewer } from '@cognite/reveal'
 import { CogniteClient } from '@cognite/sdk';
 
 const client = new CogniteClient({ appId: "reveal.example.simple" });
@@ -9,6 +9,7 @@ function App() {
   const canvasWrapper = useRef<HTMLDivElement>(null)
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [viewer, setViewer] = useState<Cognite3DViewer>();
+  const [model, setModel] = useState<Cognite3DModel>();
 
   useEffect(() => {
     const localViewer = new Cognite3DViewer({
@@ -16,7 +17,11 @@ function App() {
       domElement: canvasWrapper.current!,
     });
     setViewer(localViewer);
-    localViewer.addModel({ modelId: 5641986602571236, revisionId: 5254077049582015 })
+    (async () => {
+      const model = await localViewer.addModel({ modelId: 5641986602571236, revisionId: 5254077049582015 })
+      localViewer.fitCameraToModel(model)
+      setModel(model)
+    })()
     return () => {
       localViewer.dispose();
       console.log('viewer disposed');
@@ -26,7 +31,12 @@ function App() {
   return (
     <div>
       <h1>Hello world</h1>
-      <div ref={canvasWrapper}/>
+      <div>
+        <div style={{display: 'flex'}}>
+          <button>Use measurement</button>
+        </div>
+        <div ref={canvasWrapper} style={{maxHeight: '90vh'}}/>
+      </div>
     </div>
   );
 }
